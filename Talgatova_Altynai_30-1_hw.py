3a360b013a738f7eb47e3b3e1b329f3061c4b0b7
Altynai <134178554+AltynaiTalgatova@users.noreply.github.com>
2023-07-04 12:05:00 +0600

    Commit

---
+++ Talgatova_Altynai_30-1_hw.py
@@ -0,0 +1,30 @@
+from aiogram import executor
+import logging
+from config import dp, ADMINs, bot
+from handlers import commands, callback, extra, admin, fsm_mentor, notifications, search_music
+from database.mybot_db import sql_create
+
+
+commands.register_handlers_commands(dp)
+callback.register_handlers_callback(dp)
+admin.register_handlers_admin(dp)
+fsm_mentor.register_handlers_fsm_mentor(dp)
+extra.register_handlers_extra(dp)
+search_music.register_handlers_search_music(dp)
+
+
+async def on_startup(dp):
+    sql_create()
+    await bot.send_message(ADMINs[0], "Hello")
+    await notifications.set_scheduler()
+
+
+async def on_shutdown(dp):
+    await bot.send_message(ADMINs[0], "Bye Bye!")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True,
+                           on_startup=on_startup,
+                           on_shutdown=on_shutdown)
